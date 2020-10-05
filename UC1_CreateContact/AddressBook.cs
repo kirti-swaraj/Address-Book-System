0e179026678fa42c527eb58fc388cee338abc26e
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-05 06:56:50 +0530

    [kirti]Add . Initial Commit

---
+++ UC1_CreateContact/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        private List<Contact> addressBookList;
+        public AddressBook()
+        {
+            addressBookList = new List<Contact>();
+
+        }
+        public void AddDetailsOfPersons(string firstName, string lastName, string address, string city, string state, int zip, long phoneNo, string email)
+        {
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
+            addressBookList.Add(contact);
+        }
+
+        public void DisplayPersonDetails()
+        {
+            int numberOfPersons = 1;
+            foreach (Contact contact in addressBookList)
+
+
+            {
+
+                Console.WriteLine("The Details of Contact Number {0} -", numberOfPersons);
+                Console.WriteLine(" the first name is : " + contact.firstName + " \n the last name is :" + contact.lastName + "\n the address is : " + contact.address + " \n the city is : " + contact.city + " \n the state is " + contact.state + "\n the zip is :" + contact.zip + "\n the phone number is : " + contact.phoneNo + " \n the email is :" + contact.email);
+                numberOfPersons++;
+                
+            }
+        }
+
+
+    }
+}
---
+++ UC1_CreateContact/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddDetailsOfPersons("Kirti", "Swaraj", "home", "Nawada", "Bihar", 805110, 9854723, "kirti@gmail.com");
+            addressBook.DisplayPersonDetails();
+        }
+    }
+}
